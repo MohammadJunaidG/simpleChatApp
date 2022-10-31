4c46d393f24899f2093a62011f1f75566f14f472
MohammadJunaid <mojunaid2050@gmail.com>
2022-10-31 23:01:29 +0530

    simple-chat-app

---
+++ public/client.js
@@ -0,0 +1,58 @@
+const socket = io()
+let userName;
+let textarea = document.querySelector('#textarea')
+let username = document.querySelector('#username')
+let messageArea = document.querySelector('.message__area')
+do {
+    userName = prompt('Please enter your userName: ')
+} while(!userName);
+
+document.getElementById("username").innerHTML = "Hi! " + userName;
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+
+
